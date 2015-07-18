495b83336b68ae207afd15bae183ae091e28501e
Kishore Varma <kishorevarma.n@gmail.com>
2015-07-18 22:26:39 +1200

    route chnages for proxy

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,26 @@ var client = require('../store/client');
 
 
 
-router.get('/api/skill/list', function(req, res) {
+router.get('/skill/list', function(req, res) {
 	client.getSkillList(function(data) {
 		res.json(data);
 	});
 });
 
 
-router.get('/api/skill/:id', function(req, res) {
+router.get('/skill/:id', function(req, res) {
 	client.getSkillDetails(req.params.id, function(data) {
 		res.json(data);
 	});
 });
 
-router.get('/api/org/list', function(req, res) {
+router.get('/org/list', function(req, res) {
 	client.getOrgsList(function(data) {
 		res.json(data);
 	});	
 });
 
-router.get('/api/org/:id', function(req, res) {
+router.get('/org/:id', function(req, res) {
 	client.getOrgDetails(req.params.id,function(data) {
 		res.json(data)
 	})
